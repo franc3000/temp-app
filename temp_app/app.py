9b24335846acafa37ec8dbb1e5b446f38b126a89
Franklin <franklin.sarkett@gmail.com>
2017-09-19 00:27:35 -0500

    log

--- temp_app/app.py
+++ temp_app/app.py
@@ -6,6 +6,13 @@ from temp_app import commands, public, user
 from temp_app.extensions import bcrypt, cache, csrf_protect, db, debug_toolbar, login_manager, migrate, webpack
 from temp_app.settings import ProdConfig
 
+import logging
+
+# logging.config().dictConfig(LOGGING)
+logging.basicConfig()
+logger = logging.getLogger('MYAPP')
+logger.info("Just testing")
+
 
 def create_app(config_object=ProdConfig):
     """An application factory, as explained here: http://flask.pocoo.org/docs/patterns/appfactories/.
@@ -14,6 +21,8 @@ def create_app(config_object=ProdConfig):
     """
     app = Flask(__name__.split('.')[0])
     app.config.from_object(config_object)
+    logger.warning(config_object)
+    logger.warning(app.config)
     register_extensions(app)
     register_blueprints(app)
     register_errorhandlers(app)
@@ -44,11 +53,13 @@ def register_blueprints(app):
 
 def register_errorhandlers(app):
     """Register error handlers."""
+
     def render_error(error):
         """Render error template."""
         # If a HTTPException, pull the `code` attribute; default to 500
         error_code = getattr(error, 'code', 500)
         return render_template('{0}.html'.format(error_code)), error_code
+
     for errcode in [401, 404, 500]:
         app.errorhandler(errcode)(render_error)
     return None
@@ -56,6 +67,7 @@ def register_errorhandlers(app):
 
 def register_shellcontext(app):
     """Register shell context objects."""
+
     def shell_context():
         """Shell context objects."""
         return {
